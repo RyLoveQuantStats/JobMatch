d42db850b302241994adffb9713ef292e83f66f8
RyLoveQS <rylovequantstats@gmail.com>
2025-03-29 13:13:43 -0600

    Changes to main.py

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ from scripts.regex_extract import main as regex_extract_main
 def run_pipeline():
     # Step 1: Scrape job postings and save to database
     # Uncomment the following line to scrape jobs but only do this when necessary as all jobs are stored in the database that have been pulled previously
-    # We only have 50,000 jobs for the api limit
+    # We only have 50,000 jobs for the api limit; do not run this!
     # scrape_jobs_main()
 
     # Step 2: Update similarity scores using resume.txt
-    #resume_path = os.path.join(os.getcwd(), "resume.txt")
-    #update_tfidf_similarity(resume_path, DATABASE_FILE)
-    #update_sbert_similarity(DATABASE_FILE, resume_path)
+    resume_path = os.path.join(os.getcwd(), "resume.txt")
+    update_tfidf_similarity(resume_path, DATABASE_FILE)
+    update_sbert_similarity(DATABASE_FILE, resume_path)
 
     # Step 3: Regex extraction and Final Similarity Score Calculation
     regex_extract_main()
